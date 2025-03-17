564bb6e40c64bf380a99034b791e68a6faf0b013
zsiller <zhsiller@gmail.com>
2025-03-16 20:38:34 -0400

    add main

--- src/fuelgrainsim/thrust_simulation.py
+++ src/fuelgrainsim/thrust_simulation.py
@@ -374,8 +374,11 @@ def run_simulation(input_folder: str, output_folder: str, isp: str, a: str, nn: 
     return True
 
 
+def main():
+    dxf, svg, level, variables = file_args()
+    run_simulation(dxf, svg, variables, level)
+
 if __name__ == "__main__":
-    args = file_args()
-    run_simulation(args[0], args[1], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10])
+    main()
 
 
